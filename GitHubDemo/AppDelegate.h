57c64f3166fd55858af5e75046b7d6d3a747ffe1
wangheng <wangheng@192.168.0.113>
2016-08-12 18:00:30 +0800

    Initial Commit

---
+++ GitHubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubDemo
+//
+//  Created by 王恒 on 16/8/12.
+//  Copyright © 2016年 wangheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubDemo
+//
+//  Created by 王恒 on 16/8/12.
+//  Copyright © 2016年 wangheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
